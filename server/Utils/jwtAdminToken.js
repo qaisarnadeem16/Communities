ae4736ee9f30c50eaa229930bd8ee352cf3db3b2
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2023-09-04 01:46:56 +0500

    Resolve2

--- server/Utils/jwtAdminToken.js
+++ server/Utils/jwtAdminToken.js
@@ -1,18 +1,22 @@
-// create token and saving that in cookies
-const sendAdminToken = (user, statusCode, res) => {
-    const token = user.getJwtToken();
-    // Options for cookies
-    const options = {
-      expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-      httpOnly: true,
-      Secure: true,
-    };
-  
-    res.status(statusCode).cookie("adminToken", token, options).json({
-      success: true,
-      user,
-      token,
-    });
+const createAndSetAdminToken = (user, statusCode, res) => {
+  const token = user.getJwtToken();
+  // Options for cookies
+  const options = {
+    // The maximum age of the cookie in milliseconds.
+    maxAge: 90 * 24 * 60 * 60 * 1000,
+    // Indicates that the cookie should only be accessible to the browser.
+    httpOnly: true,
+    // Indicates that the cookie should only be sent over HTTPS.
+    secure: true,
+    // The domain that the cookie is valid for.
+    domain: "https://communities-snowy.vercel.app/",
   };
-  
-  module.exports = sendAdminToken;
+
+  res.status(statusCode).cookie("adminToken", token, options).json({
+    success: true,
+    user,
+    token,
+  });
+};
+
+module.exports = createAndSetAdminToken;
